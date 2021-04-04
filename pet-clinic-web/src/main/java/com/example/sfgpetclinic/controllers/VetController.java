01581b4be7e95671e31182c9daf29cbd3014f9b5
Russell Skalden <russell.skalden@gmail.com>
2021-04-04 22:51:33 +0500

    Implemented list of vets on vet page. Closes #15

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package com.example.sfgpetclinic.controllers;
 
+import com.example.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html"}, method = RequestMethod.GET)
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
