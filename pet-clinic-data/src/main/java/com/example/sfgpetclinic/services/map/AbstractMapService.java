3e8fb8845c0ff5228233262a55cf07b11bb6dc9b
Russell Skalden <russell.skalden@gmail.com>
2021-05-29 19:23:00 +0500

    Adding specialties to vets on startup. closes #34

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/AbstractMapService.java
@@ -37,10 +37,14 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
     }
 
     private Long getNextId() {
-        if (map.isEmpty()) {
-            return 1L;
+        Long nextId = null;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
         }
 
-        return Collections.max(map.keySet()) + 1;
+        return nextId;
     }
 }
